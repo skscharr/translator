3b7913dc37fd81672d6ed09d385a59229182c159
skscharr <skscharr@gmail.com>
2013-02-18 13:56:13 -0500

    validations working horray

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -5,12 +5,12 @@ class Challenge < ActiveRecord::Base
   
   def correct_answers
     errors.add(:q1, "incorrect") unless self.q1 == 'I'
-    errors.add(:q2, "incorrect") unless self.q2 == "that"
-    errors.add(:q3, "incorrect") unless self.q3 == "creature"
-    errors.add(:q4, "incorrect") unless self.q4 == "saw"
-    errors.add(:q5, "incorrect") unless self.q5 == "on"
-    errors.add(:q6, "incorrect") unless self.q6 == "way"
-    errors.add(:q7, "incorrect") unless self.q7 == "go"
+    errors.add(:q2, "incorrect") unless self.q2 == 'that'
+    errors.add(:q3, "incorrect") unless self.q3 == 'creature'
+    errors.add(:q4, "incorrect") unless self.q4 == 'saw'
+    errors.add(:q5, "incorrect") unless self.q5 == 'on'
+    errors.add(:q6, "incorrect") unless self.q6 == 'way'
+    errors.add(:q7, "incorrect") unless self.q7 == 'go'
   end
   
 end
